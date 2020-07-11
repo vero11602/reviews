8fa6b179dd0219898c0ba83de1360deccefc46a5
Ingrid Jiang <vero11602@gmail.com>
2020-07-11 16:50:30 +0800

    add calc len

--- reviews.py
+++ reviews.py
@@ -1,9 +1,12 @@
 data=[]
+count = 0
 with open ('reviews.txt' , 'r') as f:
 	for line in f :
 		data.append(line)
-print (len(data))
-print (len(data[0]))
+		count += 1
+		if count % 1000 == 0:
+			print (len(data))
+print ("整筆資料總數為" , len(data) , "筆")
 
 
 
